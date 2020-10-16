598802b60ea108456f08f03e77df1d01d10b6775
Erik Axelsson <e.axelsson@tutanota.com>
2020-10-16 19:31:25 +0200

    add basic login view

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,22 @@
 <template>
-  <h2>Logga In</h2>
+  
+
+  <div>
+    <h2>Logga In</h2>
+
+    <form action="" method="post" @submit.prevent="submitForm()">
+      <div>
+        <label for="">E-post</label>
+        <input type="text" v-model="email">
+      </div>
+      <div>
+        <label for="">Lösenord</label>
+        <input type="text" v-model="password">
+      </div>
+      <input type="submit" value="Login">
+      <nuxt-link to="/">Avbryt</nuxt-link>
+    </form>
+  </div>
 </template>
 
 <script>
