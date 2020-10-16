5572f0d51c1a34b23abca3c4b5d8613927cc76bc
Erik Axelsson <e.axelsson@tutanota.com>
2020-10-16 19:17:14 +0200

    add basic form in pages/registrering

--- pages/registrering.vue
+++ pages/registrering.vue
@@ -2,32 +2,41 @@
   <div>
     <h2>Registrering</h2>
 
-    <form action="" method="post" @submit.prevent="submitForm()">
+    <form action method="post" @submit.prevent="submitForm()">
       <div>
-        <label for="">Användarnamn</label>
-        <input type="text" v-model="user_name">
+        <label for>Användarnamn</label>
+        <input type="text" v-model="user_name" />
       </div>
       <div>
-        <label for="">E-post</label>
-        <input type="text" v-model="email">
+        <label for>E-post</label>
+        <input type="text" v-model="email" />
       </div>
       <div>
-        <label for="">Lösenord</label>
-        <input type="text" v-model="password">
+        <label for>Lösenord</label>
+        <input type="text" v-model="password" />
       </div>
-      <input type="submit" value="Register">
+      <input type="submit" value="Register" />
       <nuxt-link to="/">Avbryt</nuxt-link>
     </form>
   </div>
-  
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      user_name: null,
+      email: null,
+      password: null,
+    };
+  },
+  methods: {
+    submitForm() {
+      
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
