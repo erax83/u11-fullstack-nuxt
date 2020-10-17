a7781424ec2c629bc6095c9d1498d51e4e719404
Erik Axelsson <e.axelsson@tutanota.com>
2020-10-17 13:24:55 +0200

    change name on backend folder to api and add user in api/index

--- api/index.js
+++ api/index.js
@@ -8,7 +8,11 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const users = require('./routes/users')
+
+app.use(users)
+
 module.exports = {
-    path: '/backend',
+    path: '/api',
     handler: app
   }
--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,7 @@ module.exports = {
   },
 
   serverMiddleware: [
-    '~/backend/index.js'
+    '~/api/index.js'
 ]
 
 }
